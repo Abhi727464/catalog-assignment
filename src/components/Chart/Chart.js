75a0df4877ca1fafc441a6389f77ea06ddf274b4
Abhay Singh <abhaysinghparmar123@gmail.com>
2024-10-18 23:37:58 +0530

    chart error fixes

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-// import "./style.css";
 import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
 const Chart = ({ chartData }) => {
   const options = {
